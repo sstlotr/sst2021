8ce284097489ad4c2ba55af07026653900a60ac7
sstlotr <78394782+sstlotr@users.noreply.github.com>
2021-02-04 19:39:29 +0800

    Add files via upload

---
+++ all.js
@@ -0,0 +1,20 @@
+<p id="hitokoto">:D 获取中...</p>
+<script>
+    fetch('https://v1.hitokoto.cn')
+        .then(response => response.json())
+        .then(data => {
+            const hitokoto = document.getElementById('hitokoto')
+            hitokoto.innerText = data.hitokoto
+        })
+        .catch(console.error)
+</script>
+<script size="90" alpha="0.5" zIndex="0" src="https://htdfree0.htdasd.ltd/od/others/js/dongtai.js" type="text/javascript" charset="utf-8"></script>
+<script src='//cdn.jsdelivr.net/npm/valine/dist/Valine.min.js'></script>
+<div id="vcomments"></div>
+<script>
+    new Valine({
+        el: '#vcomments',
+        appId: '0HhSr8I1PFSAIc8Cl278Apt5-gzGzoHsz',
+        appKey: 'YUNGmeYwniv8hs0f03FsHkTd'
+    })
+</script>
